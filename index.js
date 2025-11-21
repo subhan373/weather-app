9dfef5efb657c0514e4f467c741102a67676ed7d
subhan373 <subhanwork132@gmail.com>
2025-11-21 20:07:20 +0500

    Hi

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const loading = document.querySelector(".loading");
 
 
 
+
+
+
+
+
+
+
+
+
+
 searchBox.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     checkWeather(searchBox.value);
