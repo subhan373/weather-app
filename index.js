10bad9844a1cbf137005253df356eba22557fe4c
subhan373 <subhanwork132@gmail.com>
2025-11-28 15:04:57 +0500

    C

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ searchBox.addEventListener("keydown", (event) => {
 
 
 
+
+
+
 async function checkWeather(city) {
   loading.style.display = "block";
   document.querySelector(".weather").style.display = "none";
